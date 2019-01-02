47c16b3a347c0ddc914d934ac32ccc3af87f172f
PrinceFroggy <asolesa@yahoo.ca>
2019-01-02 18:37:09 -0500

    Initial Commit

---
+++ nBot/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  nBot
+//
+//  Created by Andrew Solesa on 2019-01-02.
+//  Copyright © 2019 KSG. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ nBot/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  nBot
+//
+//  Created by Andrew Solesa on 2019-01-02.
+//  Copyright © 2019 KSG. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
