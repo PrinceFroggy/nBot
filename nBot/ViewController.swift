688e1bb3909cbd9542a3e5002dcef4ea9c367475
PrinceFroggy <asolesa@yahoo.ca>
2019-01-02 19:54:46 -0500

    Summoning Satan!
    I summon thee!

--- nBot/ViewController.swift
+++ nBot/ViewController.swift
@@ -7,13 +7,25 @@
 //
 
 import Cocoa
+import WebKit
+import Kanna
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var nikeWKWebView: WKWebView!
+    
+    let browserDelay = DispatchQueue(label: "browswerBackground", qos: .userInitiated)
+    
+    var delay = 3.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let urlReq = URLRequest(url: URL(string: "https://store.nike.com/ca/en_gb/pw/mens-jordan-shoes/7puZofqZoi3")!)
+        
+        self.nikeWKWebView.load(urlReq)
     }
 
     override var representedObject: Any? {
@@ -22,6 +34,42 @@ class ViewController: NSViewController {
         }
     }
 
-
+    @IBAction func buttonPressed(_ sender: NSButton)
+    {
+        AI_FirstStep_LoadCategory_FindItem()
+    }
+    
+    func AI_FirstStep_LoadCategory_FindItem()
+    {
+        DispatchQueue.main.async
+            {
+                let urlReq = URLRequest(url: URL(string: "https://store.nike.com/ca/en_gb/pw/mens-jordan-shoes/7puZofqZoi3")!)
+                
+                self.nikeWKWebView.load(urlReq)
+                
+                self.browserDelay.asyncAfter(deadline: .now() + self.delay)
+                {
+                    DispatchQueue.main.async
+                        {
+                            self.nikeWKWebView.evaluateJavaScript("document.documentElement.outerHTML.toString()", completionHandler: { (html: Any?, error: Error?) in
+                                
+                                let htmlText = html as! String
+                                
+                                var count = 0
+                                
+                                if let doc = try? Kanna.HTML(html: htmlText, encoding: .utf8)
+                                {
+                                    for item in doc.css("div[class^='grid-item fullSize']")
+                                    {
+                                        count += 1
+                                    }
+                                    
+                                    print("There are \(count) items!")
+                                }
+                                
+                                })
+                        }
+                }
+            }
+    }
 }
-
